c826a653c6d63bc08eaba8942b92436c235fde6a
jammychiou1 <jammy.chiou1@gmail.com>
2020-08-22 02:34:54 +0800

    update production api host

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     },
     plugins: [
         new webpack.DefinePlugin({
-            API_HOST: JSON.stringify('https://API')
+            API_HOST: JSON.stringify('https://hiddenntu-potato-api.herokuapp.com')
         })
     ]
 });
